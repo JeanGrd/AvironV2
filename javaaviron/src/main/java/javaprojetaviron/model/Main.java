cbe85ee335c9aefdbef60ae482a34df886a77edd
Pauline Varin <pauline.varin@toulouse.miage.fr>
2023-03-17 08:24:12 +0000

    Main.java edited online with Bitbucket

--- javaaviron/src/main/java/javaprojetaviron/model/Main.java
+++ javaaviron/src/main/java/javaprojetaviron/model/Main.java
@@ -1,89 +1,74 @@
 package javaprojetaviron.model;
 
-import java.util.ArrayList;
+import java.time.LocalDate;
 
-public class Embarcation{
-    private String nom;
-    private float vitesse;
-    private float cadence;
-    private int sizeEmbarcation;
-    private ArrayList<Participant> placementParticipant;
+public class Main {
+    public static void main(String[] args) throws Exception {
 
-    public float getVitesse() {
-        return vitesse;
+        Tournoi test = new Tournoi("test","Narbonne", "FS5X+", 75, 25, 4, TypeTournoi.COURSE_LIGNE, null);
+
+        LocalDate date = LocalDate.of(2000, 10, 26);
+
+        Participant p1 = new Participant("Jean", "Marc", date);
+        Participant p2 = new Participant("Jean", "Francois", date);
+        Participant p3 = new Participant("Jean", "Ana", date);
+        Participant p4 = new Participant("Jean", "Covici", date);
+
+        p3.getCurrentAge();
+
+        Embarcation emb1 = new Embarcation("Winner", 5);
+        emb1.positionnerParticipant(1, p1);
+        emb1.positionnerParticipant(2, p2);
+        emb1.positionnerParticipant(3, p3);
+        emb1.positionnerParticipant(4, p4);
+        emb1.putBarreur(p1);
+
+        Embarcation emb2 = new Embarcation("Jean", 5);
+        emb2.positionnerParticipant(1, p1);
+        emb2.positionnerParticipant(2, p2);
+        emb2.positionnerParticipant(3, p3);
+        emb2.positionnerParticipant(4, p4);
+        emb2.putBarreur(p1);
+
+        Embarcation emb3 = new Embarcation("Ana", 5);
+        emb3.positionnerParticipant(1, p1);
+        emb3.positionnerParticipant(2, p2);
+        emb3.positionnerParticipant(3, p3);
+        emb3.positionnerParticipant(4, p4);
+        emb3.putBarreur(p1);
+
+        Embarcation emb4 = new Embarcation("Looser", 5);
+        emb4.positionnerParticipant(1, p1);
+        emb4.positionnerParticipant(2, p2);
+        emb4.positionnerParticipant(3, p3);
+        emb4.positionnerParticipant(4, p4);
+        emb4.putBarreur(p1);
+
+        test.addConcourrant(emb1);
+        test.addConcourrant(emb2);
+        test.addConcourrant(emb3);
+        test.addConcourrant(emb4);
+
+        System.out.println(test.isOk());
+
+        //test.running();
+
+        //test.addInClassement(75, 0, emb3);
+        //test.addInClassement(75, 0, emb2);
+
+        //test.showClassement(25);
+        //test.showClassement(75);
+
+        //test.generateCSV("test");
+
+        Tournoi MyTournament = Tournoi.readTournoi("test copy 2.csv");
+
+        MyTournament.running();
+
+        MyTournament.showClassement(25);
+        MyTournament.showClassement(50);
+        MyTournament.showClassement(75);
+
+        MyTournament.generateCSV("bonjour.csv");
     }
-
-    public float getCadence() {
-        return cadence;
-    }
-
-    public int getSizeEmbarcation() {
-        return sizeEmbarcation;
-    }
-
-    public ArrayList<Participant> getPlacementParticipant() {
-        return placementParticipant;
-    }
-
-    public String getNom() {
-        return nom;
-    }
-
-    public Embarcation (String nom, int sizeEmbarcation){
-        this.nom = nom;
-        this.sizeEmbarcation = sizeEmbarcation;
-        this.placementParticipant = new ArrayList<Participant>(this.sizeEmbarcation);
-        for (int i = 0; i < this.sizeEmbarcation; i++) {
-            placementParticipant.add(null);
-        }
-    }
-
-    @Override
-    public String toString() {
-        return this.getNom();
-    }
-
-    public void positionnerParticipant (int indice, Participant participant) throws Exception{
-        if (indice < placementParticipant.size()) {
-            if (placementParticipant.get(indice) != null) {
-                throw new Exception("Il y a déjà un autre participant sur la place");
-            } else {
-                placementParticipant.set(indice, participant);
-            }
-        } else {
-            throw new Exception("L'indice est en dehors de la plage autorisée pour cette liste");
-        }
-    }
-
-    public void putBarreur(Participant participant){
-        placementParticipant.set(0, participant);
-    }
-
-    public void removeBarreur(){
-        placementParticipant.set(0, null);
-    }
-
-    public boolean containsBarreur() {
-        return this.placementParticipant.get(0) != null;
-    }
-
-    public boolean isOk () {
-        for (int i = 1; i < this.placementParticipant.size(); i++) {
-            if (this.placementParticipant.get(i) == null) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    public boolean checkAge (int age_limite) {
-        for (Participant participant : this.placementParticipant) {
-            if (participant != null) {
-                if (participant.getCurrentAge() > age_limite)
-                    return false;
-            }
-        }
-        return true;
-    }
-
 }
